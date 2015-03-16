69e49753eba18f91ce9f5c4e49f35de19a682eb9
Sambaran Roy <sambaran.roy@emids.com>
2015-03-16 07:33:16 +0530

    Modified the error key for error objects

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -5,10 +5,9 @@ module.exports = function (err, req, res, next) {
 
   if(!err) return;
   var errorResponse = {};
-  var errorString = err.message.split(':');
   errorResponse.status = err.status || err.statusCode || 400;
   errorResponse.code = err.code || errorResponse.status;
-  errorResponse.error = errorString[0];
+  errorResponse.error = err.message;
   errorResponse.message = err.message || 'Server error occurred.';
   errorResponse.stack = err.stack;
 
--- app/middleware/responseFormatter.js
+++ app/middleware/responseFormatter.js
@@ -8,8 +8,8 @@ module.exports = function(req, res, next) {
     var errorResponse = {};
     errorResponse.status = 400;
     errorResponse.code = errorResponse.status;
-    errorResponse.error = 'File Not Found';
-    errorResponse.message = 'File Not Found';
+    errorResponse.error = 'Could not decode request: File Not Found';
+    errorResponse.message = 'Could not decode request: File Not Found';
     errorResponse.stack = '';
 
     res.status(errorResponse.status).json(errorResponse); 
