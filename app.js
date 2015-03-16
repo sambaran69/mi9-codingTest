709ea9b0ad799ac3c8d988c45ecbdb25eb9ed3af
Sambaran Roy <sambaran.roy@emids.com>
2015-03-16 06:25:59 +0530

    Modifications to run the app on Heroku

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ var cors = require('cors');
 var util = require('util');
 var fs = require('fs');
 var errorHandler = require('./app/middleware/errorHandler');
-
 var routesDir = './app/routes/';
-var port = process.env.port || 3000;
 
 var app = module.exports = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static(__dirname + '/public'));
 var routes = fs.readdirSync(routesDir);
 routes.forEach(function(route) {
 	var routePath = routesDir + route;
 	require(routePath)(app, errorHandler);
 });
 
-app.listen(port, function() {
+app.listen(app.get('port'), function() {
 	console.log(util.format('Server listening on port %d.', port));
 });
