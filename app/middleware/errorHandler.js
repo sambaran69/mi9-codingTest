48a77dbb055ea8995603fbc40fcfdb6527a086bf
Sambaran Roy <sambaran.roy@emids.com>
2015-03-16 06:52:40 +0530

    Modified the error object

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -4,10 +4,11 @@ module.exports = function (err, req, res, next) {
   //'next' unused but needed for express.
   if(!err) return;
   var errorResponse = {};
+  var errorString = err.message.split(':');
   errorResponse.status = err.status || err.statusCode || 400;
   errorResponse.code = err.code || errorResponse.status;
-  errorResponse.error = err.message || 'Server error occurred.';
-  errorResponse.developerMessage = err.friendlyMessage || 'Undefined server error.';
+  errorResponse.error = errorString[0];
+  errorResponse.message = err.message || 'Server error occurred.';
   errorResponse.stack = err.stack;
 
   res.status(errorResponse.status).json(errorResponse);
